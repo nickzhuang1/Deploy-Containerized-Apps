a5cb8e279368776025097248221a4a675546b889
nick <andy345694@gmail.com>
2026-04-28 16:45:43 +0800

    [fix] add docker compose update script

---
+++ course-labs/02-docker/docker-compose-update.sh
@@ -0,0 +1,6 @@
+# 用官方腳本安裝最新版
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 \
+  -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
